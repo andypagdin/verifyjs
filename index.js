f5a3577cab4a8110313f58f86d082854eea7bd59
andypagdin <andyjpagdin@gmail.com>
2020-06-14 17:58:24 +0100

    Generate random path between start and finish in matrix
    A bit of array madness to clean up later, for now I want to get something working quickly

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Controls
 document.body.addEventListener('click', e => {
   if (e.target && e.target.nodeName == 'A') {
     e.preventDefault()
@@ -21,20 +20,112 @@ const move = direction => {
   console.log('moving', direction)
 }
 
-// Container
-const tiles = [
-  0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0
-]
+const getPossibleMoves = (position, destination, tiles) => {
+  // always move right on first move
+  if (position[1] === 0) return [[position[0], 1]]
 
-const board = document.getElementById('board')
+  // always move right on second to last move
+  if (position[1] === tiles[0].length - 2) return [[position[0], 5]]
 
-tiles.forEach((tile, index) => {
-  let node = document.createElement('div')
-  node.classList = 'tile'
-  node.dataset.index = index
+  let right = tiles[position[0]][position[1] + 1]
+  let up = tiles[position[0] - 1]
+    ? tiles[position[0] - 1][position[1]]
+    : undefined
+  let down = tiles[position[0] + 1]
+    ? tiles[position[0] + 1][position[1]]
+    : undefined
 
-  board.appendChild(node)
-})
+  let moves = []
+
+  if (right !== undefined) {
+    moves.push([ position[0], position[1] + 1 ])
+  }
+
+  // TODO: tidy last column logic
+  if (up !== undefined && up == 0) {
+    // If the current position is on the last line
+    // only move up if it is towards the destination
+    if (position[1] == destination[1]) {
+      if (position[0] - 1 >= destination[0]) {
+        moves.push([ position[0] - 1, position[1] ])
+      }
+    } else {
+      moves.push([ position[0] - 1, position[1] ])
+    }
+  }
+
+  if (down !== undefined && down == 0) {
+    // If the current position is on the last line
+    // only move down if it is towards the destination
+    if (position[1] == destination[1]) {
+      if (position[0] + 1 <= destination[0]) {
+        moves.push([ position[0] + 1, position[1] ])
+      }
+    } else {
+      moves.push([ position[0] + 1, position[1] ])
+    }
+  }
+
+  return moves
+}
+
+const getRandomNum = max => {
+  return Math.floor(Math.random() * max)
+}
+
+const generateBoardMarkup = tiles => {
+  const board = document.getElementById('board')
+
+  tiles.forEach(row => {
+    row.forEach((tile, index) => {
+      let node = document.createElement('div')
+      node.classList = 'tile'
+      node.dataset.index = index
+      node.innerHTML = tile
+
+      board.appendChild(node)
+    })
+  })
+}
+
+const generatePath = () => {
+  const tiles = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]
+
+  let position = [getRandomNum(4), 0] // x, y
+  let destination = [getRandomNum(4), 5] // x, y
+
+  let tries = 1
+  let found = false
+  tiles[position[0]][0] = tries
+
+  while (!found) {
+    tries++
+
+    // Get possible movements
+    let moves = getPossibleMoves(position, destination, tiles)
+
+    // Randomly choose a move
+    let move = moves[getRandomNum(moves.length)]
+
+    // Make the move
+    position = [move[0], move[1]]
+
+    // Update tile
+    tiles[position[0]][position[1]] = tries
+
+    // Did we reach final position?
+    if (position[0] == destination[0] && position[1] == destination[1]) {
+      found = true
+      generateBoardMarkup(tiles)
+    }
+  }
+}
+
+generatePath()
+
+// module.exports = getPossibleMoves
---
+++ index.test.js
@@ -0,0 +1,31 @@
+const getPossibleMoves = require('./index')
+
+describe('Gets correct possible moves', () => {
+  const tiles = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]
+
+  it('gets correct inital moves', () => {
+    const possibleMoves = [
+      [1, 1]
+    ]
+    expect(getPossibleMoves([1, 0], [0, 5], tiles)).toEqual(possibleMoves)
+  })
+
+  it('gets correct second moves', () => {
+    const possibleMoves = [
+      [1, 2], [0, 1], [2, 1]
+    ]
+    expect(getPossibleMoves([1, 1], [0, 5], tiles)).toEqual(possibleMoves)
+  })
+
+  it('gets correct last column move', () => {
+    const possibleMoves = [
+      [0, 5]
+    ]
+    expect(getPossibleMoves([1, 5], [0, 5], tiles)).toEqual(possibleMoves)
+  })
+})
